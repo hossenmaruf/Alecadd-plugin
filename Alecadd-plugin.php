0d624c815ca1b8bd42c87849e3946fbd3c1d9ec9
hossenmaruf <severusmaruf740@gmail.com>
2022-10-16 02:34:11 +0600

    first commit

---
+++ Alecadd-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Alecadd-plugin
+ * Description: A plugin for Learn
+ * Plugin URI: https://severusmaruf740@gmail.com
+ * Author: hossen maruf
+ * Author URI: https://severusmaruf740@gmail.com
+ * Version: 1.0
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
