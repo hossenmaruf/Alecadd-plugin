6507bdb7feb0d579f6490b78f4bff09acebd89af
hossenmaruf <severusmaruf740@gmail.com>
2022-10-17 04:29:56 +0600

    Settings Link and Admin Page

--- Alecadd-plugin.php
+++ Alecadd-plugin.php
@@ -18,15 +18,45 @@ if (!defined('ABSPATH')) {
 class AlecaddPlugin
 {
 
+    public $plugin;
+
     function __construct()
     {
-        add_action('init', array($this, 'custom_post_type'));
+        // add_action('init', array($this, 'custom_post_type'));
+        $this->plugin = plugin_basename(__FILE__);
     }
 
     function register()
     {
 
         add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+
+        add_action('admin_menu', array($this, 'add_admin_pages'));
+
+        add_filter("plugin_action_links_$this->plugin", array($this, 'settings_links'));
+    }
+
+    function settings_links($links)
+    {
+
+        $setting_link = '<a href="admin.php?page=alecadd_plugin"> Settings</a>';
+        array_push($links, $setting_link);
+
+        return $links;
+    }
+
+
+    function add_admin_pages()
+    {
+
+        add_menu_page('Alecadd Plugin', 'Alecadd', 'manage_options', 'alecadd_plugin', array($this, 'admin_index'), 'dashicons-store', 110);
+    }
+
+    function admin_index()
+    {
+        //require template
+
+        require_once plugin_dir_path(__FILE__) . 'templates/admin.php';
     }
 
 
