a40a00db7cd8e063d8cc5aca06e6e8a2000d7c01
hossenmaruf <severusmaruf740@gmail.com>
2022-10-22 04:08:28 +0600

    Plugin Structure

--- Alecadd-plugin.php
+++ Alecadd-plugin.php
@@ -21,10 +21,34 @@ if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
 
 define('PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('PLUGIN_URL', plugin_dir_url(__FILE__));
+define('PLUGIN', plugin_basename(__FILE__));
 
 
 
 if (class_exists('Inc\\Init')) {
 
     Inc\Init::register_services();
+}
+
+
+
+use Inc\Base\Activate;
+use Inc\Base\Deactivate;
+
+
+register_activation_hook(__FILE__, 'activate_alecadd_plugin');
+register_deactivation_hook(__FILE__, 'deactivate_alecadd_plugin');
+
+
+
+function activate_alecadd_plugin()
+{
+    Activate::activate();
+}
+
+function deactivate_alecadd_plugin()
+{
+
+
+    Deactivate::deactivate();
 }
