5b5958fd0f98df7fdc5e63ea63646365d2503a02
hossenmaruf <severusmaruf740@gmail.com>
2022-10-22 06:11:04 +0600

    Plugin Structure

--- inc/Pages/admin.php
+++ inc/Pages/admin.php
@@ -4,30 +4,72 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 class Admin extends BaseController
 {
 
+    public $setting;
+
+    public function __construct()
+    {
+        $this->setting = new SettingsApi();
+    }
+
     public function register()
     {
+        //  add_action('admin_menu', array($this, 'add_admin_pages'));
 
 
-        add_action('admin_menu', array($this, 'add_admin_pages'));
-    }
 
-    public function add_admin_pages()
-    {
+        $pages = [
+            [
+                'page_title' => 'Alecadd Plugin',
+                'menu_title' => 'Alecadd',
+                'capability' => 'manage_options',
+                'menu_slug' => 'alecadd_plugin',
+                'callback' => function () {
+                    echo '<h1>Alecadd Plugin</h1>';
+                },
+                'icon_url' => 'dashicons-store',
+                'position' => 110
+
+            ],
+            [
+                'page_title' => 'test Plugin',
+                'menu_title' => 'test',
+                'capability' => 'manage_options',
+                'menu_slug' => 'alecaddd_plugin',
+                'callback' => function () {
+                    echo '<h1>wtf</h1>';
+                },
+                'icon_url' => 'dashicons-store',
+                'position' => 9
+
+            ]
 
 
-        add_menu_page('Alecadd Plugin', 'Alecadd', 'manage_options', 'alecadd_plugin', array(
-            $this, 'admin_index'
-        ), 'dashicons-store', 110);
-    }
+
+        ];
 
 
-    public function admin_index()
-    {
 
-        require_once $this->plugin_path . 'templates/admin.php';
+        $this->setting->addPages($pages)->register();
     }
 }
+
+//     public function add_admin_pages()
+//     {
+
+//         add_menu_page('Alecadd Plugin', 'Alecadd', 'manage_options', 'alecadd_plugin', array(
+//             $this, 'admin_index'
+//         ), 'dashicons-store', 110);
+//     }
+
+
+//     public function admin_index()
+//     {
+
+//         require_once $this->plugin_path . 'templates/admin.php';
+//     }
+// }
