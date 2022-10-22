cc073c9b37c9d63047f4ddff0e770519df654c84
hossenmaruf <severusmaruf740@gmail.com>
2022-10-23 04:07:01 +0600

    Modular Admin Pages and Subpages

--- inc/Pages/admin.php
+++ inc/Pages/admin.php
@@ -11,18 +11,17 @@ class Admin extends BaseController
 
     public $setting;
 
+    public $pages = array();
+    public $subpages = array();
+
+
+
     public function __construct()
     {
-        $this->setting = new SettingsApi();
-    }
-
-    public function register()
-    {
         //  add_action('admin_menu', array($this, 'add_admin_pages'));
+        $this->setting = new SettingsApi();
 
-
-
-        $pages = [
+        $this->pages = [
             [
                 'page_title' => 'Alecadd Plugin',
                 'menu_title' => 'Alecadd',
@@ -32,44 +31,51 @@ class Admin extends BaseController
                     echo '<h1>Alecadd Plugin</h1>';
                 },
                 'icon_url' => 'dashicons-store',
-                'position' => 110
+                'position' => 1
 
             ],
-            [
-                'page_title' => 'test Plugin',
-                'menu_title' => 'test',
-                'capability' => 'manage_options',
-                'menu_slug' => 'alecaddd_plugin',
-                'callback' => function () {
-                    echo '<h1>wtf</h1>';
-                },
-                'icon_url' => 'dashicons-store',
-                'position' => 9
-
-            ]
-
 
 
         ];
 
 
+        $this->subpages = [
+            [
+                'parent_slug' => 'alecadd_plugin',
+                'page_title' =>  'Custom Post Type',
+                'menu_title' => 'CPT',
+                'capability' => 'manage_options',
+                'menu_slug' => 'alecadd_cpt',
+                'callback' => function () {
+                    echo '<h1>Alecadd Plugin</h1>';
+                }
+            ],
+            [
+                'parent_slug' => 'alecadd_plugin',
+                'page_title' =>  'Custom Taxonomies',
+                'menu_title' => 'Taxonomies',
+                'capability' => 'manage_options',
+                'menu_slug' => 'alecadd_taxonomies',
+                'callback' => function () {
+                    echo '<h1>Alecadd Plugin</h1>';
+                }
+            ],
+            [
+                'parent_slug' => 'alecadd_plugin',
+                'page_title' =>  'Custom Widgets',
+                'menu_title' => 'Widgets',
+                'capability' => 'manage_options',
+                'menu_slug' => 'alecadd_widgets',
+                'callback' => function () {
+                    echo '<h1>Alecadd Plugin</h1>';
+                }
+            ],
+        ];
+    }
 
-        $this->setting->addPages($pages)->register();
+    public function register()
+    {
+
+        $this->setting->addPages($this->pages)->withSubPage('Deshboard')->addSubPages($this->subpages)->register();
     }
 }
-
-//     public function add_admin_pages()
-//     {
-
-//         add_menu_page('Alecadd Plugin', 'Alecadd', 'manage_options', 'alecadd_plugin', array(
-//             $this, 'admin_index'
-//         ), 'dashicons-store', 110);
-//     }
-
-
-//     public function admin_index()
-//     {
-
-//         require_once $this->plugin_path . 'templates/admin.php';
-//     }
-// }
