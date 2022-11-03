322c115fcbbfcdf2285eed4bea5e8daa75762022
hossenmaruf <severusmaruf740@gmail.com>
2022-11-04 05:03:15 +0600

    Modular Custom Post Types

--- inc/Api/Callbacks/CptCallBacks.php
+++ inc/Api/Callbacks/CptCallBacks.php
@@ -1,38 +1,44 @@
-<?php 
+<?php
+
+/**
+ * @package  AlecaddPlugin
+ */
 
 namespace Inc\Api\Callbacks;
+
 
 class CptCallbacks
 {
 
-	public function cptSectionManager()
-	{
-		echo 'Create as many Custom Post Types as you want.';
-	}
+    public function cptSectionManager()
+    {
+        echo 'Manage your customs post type';
+    }
 
-	public function cptSanitize( $input )
-	{
-		return $input;
-	}
 
-	public function textField( $args )
-	{
-		$name = $args['label_for'];
-		$option_name = $args['option_name'];
-		$input = get_option( $option_name );
-		$value = $input[$name];
+    public function cptSanitize($input)
+    {
+        return $input;
+    }
 
-		echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="' . $value . '" placeholder="' . $args['placeholder'] . '">';
-	}
 
-	public function checkboxField( $args )
-	{
-		$name = $args['label_for'];
-		$classes = $args['class'];
-		$option_name = $args['option_name'];
-		$checkbox = get_option( $option_name );
-		$checked = isset($checkbox[$name]) ? ($checkbox[$name] ? true : false) : false;
+    public function textField($args)
+    {
 
-		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ( $checked ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
-	}
+        return $args;
+    }
+
+
+
+
+    public function checkboxField($args)
+    {
+    	$name        = $args['label_for'];
+    	$classes     = $args['class'];
+    	$option_name = $args['option_name'];
+    	$checkbox    = get_option($option_name);
+    	$checked     = isset($checkbox[$name]) ? ($checkbox[$name] ? true : false) : false;
+
+    	echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ($checked ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
+    }
 }
