759d0f06b3451ad6aaf15aab9cdc0062029701dc
hossenmaruf <severusmaruf740@gmail.com>
2022-11-12 02:12:14 +0600

    Edit a custom post type

--- inc/Api/Callbacks/CptCallBacks.php
+++ inc/Api/Callbacks/CptCallBacks.php
@@ -1,9 +1,7 @@
-<?php
-
+<?php 
 /**
  * @package  AlecaddPlugin
  */
-
 namespace Inc\Api\Callbacks;
 
 class CptCallbacks
@@ -14,17 +12,17 @@ class CptCallbacks
 		echo 'Create as many Custom Post Types as you want.';
 	}
 
-	public function cptSanitize($input)
+	public function cptSanitize( $input )
 	{
 		$output = get_option('alecadd_plugin_cpt');
 
-		if (isset($_POST["remove"])) {
+		if ( isset($_POST["remove"]) ) {
 			unset($output[$_POST["remove"]]);
 
 			return $output;
 		}
 
-		if (count($output) == 0) {
+		if ( count($output) == 0 ) {
 			$output[$input['post_type']] = $input;
 
 			return $output;
@@ -37,26 +35,36 @@ class CptCallbacks
 				$output[$input['post_type']] = $input;
 			}
 		}
-
+		
 		return $output;
 	}
 
-	public function textField($args)
+	public function textField( $args )
 	{
 		$name = $args['label_for'];
 		$option_name = $args['option_name'];
-		$input = get_option($option_name);
+		$value = '';
 
-		echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="" placeholder="' . $args['placeholder'] . '" required>';
+		if ( isset($_POST["edit_post"]) ) {
+			$input = get_option( $option_name );
+			$value = $input[$_POST["edit_post"]][$name];
+		}
+
+		echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="' . $value . '" placeholder="' . $args['placeholder'] . '" required>';
 	}
 
-	public function checkboxField($args)
+	public function checkboxField( $args )
 	{
 		$name = $args['label_for'];
 		$classes = $args['class'];
 		$option_name = $args['option_name'];
-		$checkbox = get_option($option_name);
+		$checked = false;
 
-		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class=""><label for="' . $name . '"><div></div></label></div>';
+		if ( isset($_POST["edit_post"]) ) {
+			$checkbox = get_option( $option_name );
+			$checked = isset($checkbox[$_POST["edit_post"]][$name]) ?: false;
+		}
+
+		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ( $checked ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
 	}
 }
