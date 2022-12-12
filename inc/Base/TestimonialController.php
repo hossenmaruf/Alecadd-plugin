ec91da89bcfbb399d7b4abc5619b4771b496ad93
hossenmaruf <severusmaruf740@gmail.com>
2022-12-13 00:51:34 +0600

    Shortcode for testimonial

--- inc/Base/TestimonialController.php
+++ inc/Base/TestimonialController.php
@@ -4,20 +4,24 @@ namespace Inc\Base;
 
 use Inc\Api\SettingsApi;
 use Inc\Base\BaseController;
-use Inc\Api\Callbacks\AdminCallbacks;
+use Inc\Api\Callbacks\TestimonialCallbacks;
 
 /**
 * 
 */
 class TestimonialController extends BaseController
 {
-	public $callbacks;
+	public $settings;
 
-	public $subpages = array();
+	public $callbacks;
 
 	public function register()
 	{
 		if ( ! $this->activated( 'testimonial_manager' ) ) return;
+
+		$this->settings = new SettingsApi();
+
+		$this->callbacks = new TestimonialCallbacks();
 
 		add_action( 'init', array( $this, 'testimonial_cpt' ) );
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
@@ -25,23 +29,41 @@ class TestimonialController extends BaseController
 		add_action( 'manage_testimonial_posts_columns', array( $this, 'set_custom_columns' ) );
 		add_action( 'manage_testimonial_posts_custom_column', array( $this, 'set_custom_columns_data' ), 10, 2 );
 		add_filter( 'manage_edit-testimonial_sortable_columns', array( $this, 'set_custom_columns_sortable' ) );
+
+		$this->setShortcodePage();
+	}
+
+	public function setShortcodePage()
+	{
+		$subpage = array(
+			array(
+				'parent_slug' => 'edit.php?post_type=testimonial',
+				'page_title' => 'Shortcodes',
+				'menu_title' => 'Shortcodes',
+				'capability' => 'manage_options',
+				'menu_slug' => 'alecadd_testimonial_shortcode',
+				'callback' => array( $this->callbacks, 'shortcodePage' )
+			)
+		);
+
+		$this->settings->addSubPages( $subpage )->register();
 	}
 
 	public function testimonial_cpt ()
 	{
 		$labels = array(
-			'name'          => 'Testimonials',
+			'name' => 'Testimonials',
 			'singular_name' => 'Testimonial'
 		);
 
 		$args = array(
-			'labels'              => $labels,
-			'public'              => true,
-			'has_archive'         => false,
-			'menu_icon'           => 'dashicons-testimonial',
+			'labels' => $labels,
+			'public' => true,
+			'has_archive' => false,
+			'menu_icon' => 'dashicons-testimonial',
 			'exclude_from_search' => true,
-			'publicly_queryable'  => false,
-			'supports'            => array( 'title', 'editor' )
+			'publicly_queryable' => false,
+			'supports' => array( 'title', 'editor' )
 		);
 
 		register_post_type ( 'testimonial', $args );
@@ -63,9 +85,9 @@ class TestimonialController extends BaseController
 	{
 		wp_nonce_field( 'alecadd_testimonial', 'alecadd_testimonial_nonce' );
 
-		$data     = get_post_meta( $post->ID, '_alecadd_testimonial_key', true );
-		$name     = isset($data['name']) ? $data['name'] : '';
-		$email    = isset($data['email']) ? $data['email'] : '';
+		$data = get_post_meta( $post->ID, '_alecadd_testimonial_key', true );
+		$name = isset($data['name']) ? $data['name'] : '';
+		$email = isset($data['email']) ? $data['email'] : '';
 		$approved = isset($data['approved']) ? $data['approved'] : false;
 		$featured = isset($data['featured']) ? $data['featured'] : false;
 		?>
@@ -124,8 +146,8 @@ class TestimonialController extends BaseController
 		}
 
 		$data = array(
-			'name'     => sanitize_text_field( $_POST['alecadd_testimonial_author'] ),
-			'email'    => sanitize_text_field( $_POST['alecadd_testimonial_email'] ),
+			'name' => sanitize_text_field( $_POST['alecadd_testimonial_author'] ),
+			'email' => sanitize_text_field( $_POST['alecadd_testimonial_email'] ),
 			'approved' => isset($_POST['alecadd_testimonial_approved']) ? 1 : 0,
 			'featured' => isset($_POST['alecadd_testimonial_featured']) ? 1 : 0,
 		);
@@ -135,25 +157,25 @@ class TestimonialController extends BaseController
 	public function set_custom_columns($columns)
 	{
 		$title = $columns['title'];
-		$date  = $columns['date'];
+		$date = $columns['date'];
 		unset( $columns['title'], $columns['date'] );
 
-		$columns['name']     = 'Author Name';
-		$columns['title']    = $title;
+		$columns['name'] = 'Author Name';
+		$columns['title'] = $title;
 		$columns['approved'] = 'Approved';
 		$columns['featured'] = 'Featured';
-		$columns['date']     = $date;
+		$columns['date'] = $date;
 
 		return $columns;
 	}
 
 	public function set_custom_columns_data($column, $post_id)
 	{
-		$data     = get_post_meta( $post_id, '_alecadd_testimonial_key', true );
-		$name     = isset($data['name']) ? $data['name'] : '';
-		$email    = isset($data['email']) ? $data['email'] : '';
-		$approved = isset($data['approved']) && $data['approved'] == = 1 ? '<strong>YES</strong>' : 'NO';
-		$featured = isset($data['featured']) && $data['featured'] == = 1 ? '<strong>YES</strong>' : 'NO';
+		$data = get_post_meta( $post_id, '_alecadd_testimonial_key', true );
+		$name = isset($data['name']) ? $data['name'] : '';
+		$email = isset($data['email']) ? $data['email'] : '';
+		$approved = isset($data['approved']) && $data['approved'] === 1 ? '<strong>YES</strong>' : 'NO';
+		$featured = isset($data['featured']) && $data['featured'] === 1 ? '<strong>YES</strong>' : 'NO';
 
 		switch($column) {
 			case 'name':
@@ -172,7 +194,7 @@ class TestimonialController extends BaseController
 
 	public function set_custom_columns_sortable($columns)
 	{
-		$columns['name']     = 'name';
+		$columns['name'] = 'name';
 		$columns['approved'] = 'approved';
 		$columns['featured'] = 'featured';
 
